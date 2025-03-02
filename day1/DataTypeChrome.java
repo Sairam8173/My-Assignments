5ab0793f25d5ed7491d10c18c8193895ea8db659
Sairam8173 <jsairam8173@gmail.com>
2025-03-02 14:02:58 +0530

    Add files via upload
    Day1 Assignments

---
+++ day1/DataTypeChrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class DataTypeChrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float chromeVersion = 91.0f;
+		
+		String developer = "Google";
+		
+		boolean isBeta = false;
+		
+		int releaseYear = 2008;
+		
+		char shortCut = 'C';
+		
+		System.out.println("ChromeVersion is:"+chromeVersion+" "+"Developer:"+developer+" "+"isBeta:"+isBeta+" "+"Was Release in the Year:"+releaseYear+" and the "+"ShortCut:"+shortCut);
+		
+	}
+
+}
---
+++ day1/DataTypeFirefox.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class DataTypeFirefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float browserVersion = 100.2f;
+		
+		String browserName = "firefox";
+		
+		boolean isVisible = true;
+		
+		int releaseYear = 1998;
+		
+		char browserLogo = 'f';
+		
+		System.out.println("BrowserVersion is:"+browserVersion+" "+"BrowserName:"+browserName+" "
+				+"isBrowserVisible:"+true+" "+"Was released in the year:"+releaseYear+" and "+"BrowseLogo:"+browserLogo+".");
+		
+	}
+
+}
